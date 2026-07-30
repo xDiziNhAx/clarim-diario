98b8bdbc1fec9ef955545df724117da6c843cae8
xDiziNhAx <didi14rodrigues@bol.com.br>
2026-07-30 20:17:47 -0300

    ajustando p vercel 1

--- src/pages/materia/materia.jsx
+++ src/pages/materia/materia.jsx
@@ -1,9 +1,9 @@
 import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { buscarNoticia } from '../../services/noticias'
-import './Materia.css'
+import './materia.css'
 
-function Materia() {
+function materia() {
   const { id } = useParams()
 
   const [noticia, setNoticia] = useState(null)
@@ -56,4 +56,4 @@ function Materia() {
   )
 }
 
-export default Materia
+export default materia
